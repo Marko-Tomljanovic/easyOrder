932ee2da13192cdd2616057db6e1818f39121fe2
Marko-Tomljanovic <markot@windowslive.com>
2023-11-01 10:46:21 +0100

    feat: impl redux

--- easy-order-ui/src/app/page.tsx
+++ easy-order-ui/src/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import Input from "antd/es/input/Input";
+import { AppDispatch, useAppSelector } from "./redux/store";
+import { useDispatch } from "react-redux";
+import { logIn } from "./redux/features/auth-slice";
+
 export default function Home() {
+  const dispatch = useDispatch<AppDispatch>();
+  const username = useAppSelector((state) => {
+    return state.authReducer.value.username;
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(logIn(e.target.value));
+  };
   return (
     <main>
-      <div>marko</div>
+      <h3>Uspostavljen redux</h3>
+      <div style={{ width: "100px", marginTop: "200px" }}>
+        <Input onChange={handleChange} />
+      </div>
+      <div>username: {username} </div>
     </main>
   );
 }
