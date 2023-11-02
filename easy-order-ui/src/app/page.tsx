cf72019630f446053ccedbb13cdbb817c0ac62ca
Marko-Tomljanovic <markot@windowslive.com>
2023-11-02 16:14:51 +0100

    fix: typo import component

--- easy-order-ui/src/app/page.tsx
+++ easy-order-ui/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import LogIn from "./components/auth/logIn";
+import LogIn from "./components/auth/LogIn";
 
 export default function Home() {
   return (
