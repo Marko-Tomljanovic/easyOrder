0645637ce38332eca1da79b4990a75abb1dd44b2
Marko-Tomljanovic <markot@windowslive.com>
2023-11-02 16:21:34 +0100

    fix: refacotr dashboard

--- easy-order-ui/src/app/dashboard/page.tsx
+++ easy-order-ui/src/app/dashboard/page.tsx
@@ -4,20 +4,20 @@ import { logIn } from "../redux/features/auth-slice";
 import { Input } from "antd";
 
 export default function Page() {
-  const dispatch = useDispatch<AppDispatch>();
-  const username = useAppSelector((state) => {
-    return state.authReducer.value.username;
-  });
+  // const dispatch = useDispatch<AppDispatch>();
+  // const username = useAppSelector((state) => {
+  //   return state.authReducer.value.username;
+  // });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(logIn(e.target.value));
-  };
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   dispatch(logIn(e.target.value));
+  // };
   return (
     <>
-      <div style={{ width: "10rem" }}>
+      {/* <div style={{ width: "10rem" }}>
         <Input onChange={handleChange} />
       </div>
-      <div>username: {username} </div>
+      <div>username: {username} </div> */}
     </>
   );
 }
