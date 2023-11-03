1e5b152c668cb30060ff5d906d77b9f2c8bb3335
Marko-Tomljanovic <markot@windowslive.com>
2023-11-03 17:31:50 +0100

    fix: deploy error; defind localstorage

--- easy-order-ui/src/app/components/navigation/sidebar/SidebarNavigation.tsx
+++ easy-order-ui/src/app/components/navigation/sidebar/SidebarNavigation.tsx
@@ -2,13 +2,24 @@ import { Layout } from "antd";
 import React from "react";
 import AuthMenu from "./AuthMenu";
 import HomeMenu from "./HomeMenu";
-import { useAppSelector } from "@/app/redux/store";
+// import { useAppSelector } from "@/app/redux/store";
 
-const SidebarNavigation = () => {
-  const user = localStorage.getItem("isCurrentUser");
-  const username = useAppSelector((state) => {
-    return state.authReducer.value.username;
-  });
+export default function SidebarNavigation() {
+  let user = "";
+  if (typeof window !== "undefined") {
+    const isCurrentUser = localStorage.getItem("isCurrentUser");
+    if (isCurrentUser) {
+      try {
+        user = JSON.parse(isCurrentUser);
+      } catch (error) {
+        console.error("Error parsing user data from localStorage:", error);
+      }
+    }
+  }
+
+  // const username = useAppSelector((state) => {
+  //   return state.authReducer.value.username;
+  // });
 
   const { Sider } = Layout;
   return (
@@ -26,6 +37,4 @@ const SidebarNavigation = () => {
       {user ? <HomeMenu /> : <AuthMenu />}
     </Sider>
   );
-};
-
-export default SidebarNavigation;
+}
