7165066fe42404ebc9789ee8dd6098833e66e1f6
Marko-Tomljanovic <markot@windowslive.com>
2023-11-03 23:20:28 +0100

    feat: logOut button

--- easy-order-ui/src/app/components/navigation/sidebar/HomeMenu.tsx
+++ easy-order-ui/src/app/components/navigation/sidebar/HomeMenu.tsx
@@ -4,6 +4,7 @@ import {
   DatabaseOutlined,
   HomeOutlined,
   LineChartOutlined,
+  LogoutOutlined,
 } from "@ant-design/icons";
 import { useRouter } from "next/navigation";
 
@@ -12,6 +13,11 @@ export default function HomeMenu() {
   const onClick: MenuProps["onClick"] = (e) => {
     if (e.key === "3") router.push("/dashboard");
     if (e.key === "4") router.push("/dashboard");
+    if (e.key === "5") router.push("/dashboard");
+    if (e.key === "6") {
+      localStorage.removeItem("isCurrentUser");
+      router.push("/");
+    }
   };
   return (
     <Menu
@@ -24,6 +30,12 @@ export default function HomeMenu() {
         { key: "3", icon: <HomeOutlined />, label: "Dashboard" },
         { key: "4", icon: <DatabaseOutlined />, label: "Stolovi" },
         { key: "5", icon: <LineChartOutlined />, label: "Statistika" },
+        {
+          key: "6",
+          icon: <LogoutOutlined />,
+          style: { position: "absolute", bottom: "1rem" },
+          label: "LogOut",
+        },
       ]}
     />
   );
